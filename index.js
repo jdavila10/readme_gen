dbff62927be48f29b527e34938689b03f72d89f2
Jonathan Echevarria <j.eche589@gmail.com>
2020-08-13 22:50:53 -0400

    Added license choices and util

--- index.js
+++ index.js
@@ -1,44 +1,181 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
-inquirer.prompt([
-	{
-		name: 'title',
-		message: 'What is the title of your project?',
-		type: 'input'
-	},
-	{
-		name: 'licence',
-		message: 'What is licence?',
-		type: 'input'
-	},
-	{
-		name: 'description',
-		message: 'What is the description of your project?',
-		type: 'input'
-    },
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFile = util.promisify(fs.writeFile);
+//This function creates an array of questions for the user 
+function promptUser() {
+  return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project? (This will be the header of the README)"
+        },
+        {
+            type: 'input',
+            name: 'image',
+            message: 'Add the file path to an image for your project'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a description of this project'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How does a user install your application? Please provide instructions here'
+        },
+        {
+            type:  'list',
+            name: 'license',
+            message: 'Which license will you be using for your project?',
+            choices: [
+                'MIT',
+                'GNU GPLv3',
+                'Apache',
+                'Mozilla'
+            ]
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Who contributed in this project?'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please provide some instructions for the test'
+        },
+        {
+            type: 'input',
+            name: 'contact',
+            message: 'What is your Github username?'
+        },
+
+    ]);
+};
+function genReadMe(answers) {
+    return `
+
+  # ${answers.title}
+
+  ---
+
+  ![](${answers.image})
+
+  ---
+
+  ## Table of Contents
+
+  - [Image](#image)
+  - [Description](#description)
+  - [Installation](#installation)
+  - [License](#license)
+  - [Contributors](#contributors)
+  - [Tests](#tests)
+  - [Contact](#contact)
+
+  ---  
+  
+  ## Description
+
+  ${answers.description}
+
+  ---
+  
+  ## Installation
+
+  ## ${answers.installation}
+
+  ---
+
+  ## License
+
+  ${answers.license}
+
+  ---
+
+  ## Contributors
+
+  ${answers.contributors}
+
+  ---
+
+  ## Test
+
+  ${answers.test}
+
+  ---
     
-]).then((answers) => {
-	let val = '';
-	for (const key in answers) {
-		console.log();
-		if(answers[key]){
-			if (key == 'title'){
-				val += '##' + answers[key] + '\n'
-			} else if (key == 'licence'){
-				val += '##' + answers[key] + '\n'
-            }else if (key == 'licence'){
-				val += '##' + answers[key] + '\n'
-			}else {
-				val += answers[key] + '\n'
-			}
-		}
-	}
-	fs.writeFile('README.md', val, (error) => {
-		if(error){
-			console.log(error);
-		}
-		else{
-			console.log('success');
-		}
-	})
-})
+  
+  ## Contact:
+
+  For questions about my README generator please go to my Github page following this link: 
+  - [Github Profile](https://github.com/${answers.contact})
+  
+    `;
+}
+
+async function init() {
+    console.log('hello');
+
+    try {
+        const answers = await promptUser();
+
+        const readMe = genReadMe(answers);
+
+        await writeFile("README.md", readMe);
+
+        console.log("Successfully created a README file");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+init();
+
+
+
+
+
+
+
+
+
+
+// 	let val = '';
+// 	for (const key in answers) {
+// 		console.log();
+// 		if(answers[key]){
+// 			if (key == 'title'){
+// 				val += '## ' + '\n' + answers[key]
+// 			} else if (key == 'licence'){
+// 				val += '##' + answers[key] + '\n'
+//             }else if (key == 'licence'){
+// 				val += '##' + answers[key] + '\n'
+//             }else if (key == 'licence'){
+// 				val += '##' + answers[key] + '\n'
+//             }else if (key == 'licence'){
+// 				val += '##' + answers[key] + '\n'
+//             }else if (key == 'licence'){
+// 				val += '##' + answers[key] + '\n'
+//             }else if (key == 'licence'){
+// 				val += '##' + answers[key] + '\n'
+//             }else if (key == 'licence'){
+// 				val += '##' + answers[key] + '\n'
+//             }else {
+// 				val += answers[key] + '\n'
+// 			}
+// 		}
+// 	}
+// 	fs.writeFile('README.md', val, (error) => {
+// 		if(error){
+// 			console.log(error);
+// 		}
+// 		else{
+// 			console.log('You have successfully created a README file');
+// 		}
+// 	})
+// })
